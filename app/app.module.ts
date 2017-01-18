4001b97b3022d931cb52a782e2e1d844080e9623
thuydotp <phuongthuydt0311@gmail.com>
2017-01-19 00:37:13 +0700

    Roof Routing - Admin (done) - Global (will do)

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,19 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+/* Feature Modules */
+import { AdminModule }    from './admin/admin.module';
+
+/* Routing Module */
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+    BrowserModule,
+    AppRoutingModule,
+    AdminModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
